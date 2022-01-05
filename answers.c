9c7753b3f5a8f2c4e4ca16a18d3ecaa6e524e1ed
chairulridjaal <82056877+chairulridjaal@users.noreply.github.com>
2022-01-05 07:22:51 +0100

    Create answers.c

---
+++ answers.c
@@ -0,0 +1,11 @@
+sort1 uses: bubble sort
+
+How do you know?: Bubble sort goes through all the data and determine which is highest and then move it to the end, it works fast on reversed data
+
+sort2 uses: merge sort
+
+How do you know?: Merge sort cuts the problem in half and then sort it, it's the fastest when given an already sorted data
+
+sort3 uses: selection sort
+
+How do you know?: Selection sort goes through all the data and determine which is highest, it was the slowest on random data
